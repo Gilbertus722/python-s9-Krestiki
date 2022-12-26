d4b21b2a22ce074e670f1d83c7a17e202617b19d
Gilbertus722 <112508781+Gilbertus722@users.noreply.github.com>
2022-12-26 19:23:47 +0300

    Крестики-нолики

---
+++ strings.py
@@ -0,0 +1,16 @@
+BOT_TOKEN_FILENAME = 'token.txt'
+
+
+SYMBOL_X = 'X'
+SYMBOL_O = 'O'
+SYMBOL_UNDEF = '*'
+
+
+ANSW_YOUR_TURN = 'Ваш ход'
+ANSW_YOU_WIN = 'Ваша победа'
+ANSW_BOT_WIN = 'Terminator win!'
+ANSW_DRAW = 'Дружба'
+ANSW_HELP = '/new_game для начала новой игры'
+
+
+ALERT_CANNOT_MOVE_TO_THIS_CELL = 'Нажимать можно только на ' + SYMBOL_UNDEF
